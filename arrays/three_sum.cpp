7292d909f9ec20176bf19e9a01691da7f785732d
Aman Kumar <aman58320@gmail.com>
2026-09-20 15:37:42 +0530

    Three Sum

--- arrays/three_sum.cpp
+++ arrays/three_sum.cpp
@@ -30,6 +30,79 @@ void threeSumBrute(vector<int> &arr)
         cout << endl;
     }
 }
+void threeSumBetter(vector<int> &arr)
+{
+    int n = arr.size();
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++)
+    {
+        set<int> s;
+        for (int j = i + 1; j < n; j++)
+        {
+            int rem = -(arr[i] + arr[j]);
+            if (s.find(rem) != s.end())
+            {
+                vector<int> temp = {arr[i], arr[j], rem};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            s.insert(arr[j]);
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    for (auto it : ans)
+    {
+        for (auto i : it)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+void threeSumOptimal(vector<int> arr)
+{
+    sort(arr.begin(), arr.end());
+    vector<vector<int>> ans;
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0 && arr[i] == arr[i - 1])
+            continue;
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k)
+        {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0)
+            {
+                j++;
+            }
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+            {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while (j < k && arr[j] == arr[j - 1])
+                    j++;
+                while (j < k && arr[k] == arr[k + 1])
+                    k--;
+            }
+        }
+    }
+    for (auto it : ans)
+    {
+        for (auto i : it)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     int n;
@@ -40,6 +113,6 @@ int main()
         cin >> arr[i];
     }
     cout << "-------" << endl;
-    threeSumBrute(arr);
+    threeSumOptimal(arr);
     return 0;
 }
